e8823d9287eaae3dab362d27f24e84a8d103c337
Marcelo0920 <marcelo0920.z@gmail.com>
2024-11-13 23:41:47 -0400

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,45 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./components/login/login.component').then(
+        (m) => m.LoginComponent
+      ),
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./components/dashboard/dashboard.component').then(
+        (m) => m.DashboardComponent
+      ),
+    canActivate: [authGuard],
+    children: [
+      { path: '', redirectTo: 'clients', pathMatch: 'full' },
+      {
+        path: 'clients',
+        loadComponent: () =>
+          import('./components/client-list/client-list.component').then(
+            (m) => m.ClientListComponent
+          ),
+      },
+      {
+        path: 'products',
+        loadComponent: () =>
+          import('./components/product-list/product-list.component').then(
+            (m) => m.ProductListComponent
+          ),
+      },
+      {
+        path: 'students',
+        loadComponent: () =>
+          import('./components/student-list/student-list.component').then(
+            (m) => m.StudentListComponent
+          ),
+      },
+    ],
+  },
+];
